c971293049250021ac13aaf9f83abfaef7a13284
vcannataro <vcannataro@ufl.edu>
2015-01-29 21:41:32 -0500

    Starting off

---
+++ HW_dynamics_master.R
@@ -0,0 +1,13 @@
+################
+#H-W population dynamics simulation
+#Author: Vincent Cannataro
+#WORK IN PROGRESS
+################
+
+#Number of each genotype in the population initially 
+AA <- 1000
+Aa <- 1000
+aa <- 1000
+
+Generations <- 100 
+
---
+++ HW_dynamics_sim.R
@@ -0,0 +1,35 @@
+##################
+#Code that controls evolution
+#Author: Vincent Cannataro
+#WORK IN PROGRESS
+##################
+
+#The total population size of the individuals
+Total.pop.size <- AA+Aa+aa
+
+#Matrix to keep track of genotypes over the generations
+evolution.matrix <- matrix(nrow=3,ncol=Generations)
+rownames(evolution.matrix) <- c("AA","Aa","aa")
+colnames(evolution.matrix) <- paste("Generation",1:Generations)
+evolution.matrix[,1] <- c(AA,Aa,aa) #initialize the matrix
+
+
+
+##Loop that simulations evolution. Children choose their parents, allele combinations after that are random
+for(i in 2:Generations){
+  for(j in 1:Total.pop.size){
+    
+    
+    
+    
+  }
+  
+  
+  
+}
+
+
+
+
+
+
